eeb017a09bb494beffa568a23d060c8f0c661b81
Gavis33 <gagan.g.poojari@gmail.com>
2025-04-10 14:17:45 +0530

    logging and eception functionalities added

---
+++ src/exception/__init__.py
@@ -0,0 +1,58 @@
+import sys
+import logging
+
+def error_message_detail(error: Exception, error_detail: sys) -> str:
+    """
+    Extracts detailed error information from an exception and its traceback.
+    including the error message, error type, and traceback details ie file name, line number, etc.
+    Args:
+        error (Exception): The exception object.
+        error_detail (sys): The traceback object.
+
+    Returns:
+        str: A string containing the error message and traceback details.
+    """
+
+    # Extracting the traceback information (exception type, error message, and traceback details)
+    _, _, exc_tb = error_detail.exc_info()
+
+    # Get the filename where the exception occured
+    file_name = exc_tb.tb_frame.f_code.co_filename
+
+    # Create a formatted error message with file name, line number, and error message
+    line_number = exc_tb.tb_lineno
+    error_message = f'Error occured in python script name [{file_name}] at line number [{line_number}]: Error message [{str(error)}]'
+
+    # Log the error for better tracking
+    logging.info(error_message)
+
+    return error_message
+
+class MyException(Exception):
+    """
+    Custom exception class for handling errors in the US visa application.
+    """
+    def __init__(self, error_message: str, error_detail: sys):
+        """
+        Initializes a new instance of the MyException class with a detailed error message and traceback.
+
+        Args:
+            error_message (str): A detailed error message.
+            error_detail (sys): The traceback object.
+
+        Returns:
+            None
+        """
+        # Call the parent class constructor with the error message
+        super().__init__(error_message)
+
+        # Format the error message with file name, line number, and error message
+        self.error_message = error_message_detail(error_message, error_detail)
+
+    def __str__(self) -> str:
+        """
+        Returns a string representation of the MyException object.
+        Returns:
+            str: A formatted error message.
+        """
+        return self.error_message
---
+++ src/logger/__init__.py
@@ -0,0 +1,45 @@
+import os
+import logging
+from datetime import datetime
+from from_root import from_root
+from logging.handlers import RotatingFileHandler
+
+# Constants for logging configuration
+LOG_DIR = 'logs' # Directory to store log files
+LOG_FILE = f'{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log' # Log file name
+MAX_LOG_SIZE = 5 * 1024 * 1024 # Maximum log file size in bytes (5 MB)
+BACKUP_COUNT = 3 # Number of backup log files to keep
+
+# Create log directory if it doesn't exist
+log_dir_path = os.path.join(from_root(), LOG_DIR)
+os.makedirs(log_dir_path, exist_ok=True)
+log_file_path = os.path.join(log_dir_path, LOG_FILE)
+
+def configure_logger():
+    """
+    Configure logging with a rotating file handler(rotation means that the log file will be rotated when it reaches a certain size) and a console handler.
+    """
+
+    # Create a custom logger with the specified name
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+
+    # Define formatter
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    # Define handlers for console and file logging with rotation
+    # File handler
+    file_handler = RotatingFileHandler(log_file_path, maxBytes=MAX_LOG_SIZE, backupCount=BACKUP_COUNT)
+    file_handler.setFormatter(formatter)
+    file_handler.setLevel(logging.INFO)
+    # Console handler
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+    console_handler.setLevel(logging.DEBUG)
+
+    # Add handlers to the logger that means that the logger will use these handlers to log messages
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
+
+# Call the configure_logger function to apply the configuration
+configure_logger()
