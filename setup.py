459596e941f773b38f65f67d335447d909b42abf
Gavis33 <gagan.g.poojari@gmail.com>
2025-04-10 11:31:22 +0530

    Basic skeleton of the project created along with venv

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='src',
+    version='0.0.1',
+    author='Gavis',
+    author_email='gavis@gmail.com',
+    packages=find_packages(), # this will find all the packages in the src folder
+)
